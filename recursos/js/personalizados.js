991a29c1cb5187ef64cf14be9db727e597be4321
jakolab <alejo.jko@gmail.com>
2016-04-26 18:31:48 -0500

    Se incorpora la dashboard y se organizan los estilos del menú

--- recursos/js/personalizados.js
+++ recursos/js/personalizados.js
@@ -2,4 +2,25 @@ jQuery(function(){
 	jQuery(".pretty-file-input").fileinput({
 	    language: "es",
 	});
+	jQuery("aside .list-group ul").css("display", "none");
+
+	jQuery("aside .list-group .list-group-item").click(function(e){		
+		e.stopPropagation();
+		var ul = jQuery(this).find("ul");
+
+		if(jQuery(this).hasClass("open")){
+			jQuery(this).removeClass("open");
+		} else {
+			jQuery(this).addClass("open");
+		}
+
+		if(ul.length){
+			ul.slideToggle();			
+			return false;
+		}
+	});
+
+	jQuery("aside .list-group .list-group-item ul a").click(function(e){
+		e.stopPropagation();		
+	});
 });
